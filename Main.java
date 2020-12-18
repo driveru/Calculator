300069fbf264e6ca59e1008cc5a283e3fa2c6db2
kasatinka <71015825+kasatinka@users.noreply.github.com>
2020-12-18 19:38:06 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,9 @@
-public class Main{
+public class Main implements iPerson{
   public static void main(String[] args){
     System.out.println("Hello world!");
   }
 }
+
+public interface iPerson{
+  void рыгать;  
+{
